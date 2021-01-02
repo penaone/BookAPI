bb2914ebb830151d08b2edaa72552eb11fd18206
penaone <penaone@gmail.com>
2021-01-02 15:19:52 -0700

    completed backend

--- server/server.js
+++ server/server.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const path = require('path');
+const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
 //const routes = require('./routes');
 const { ApolloServer } = require('apollo-server-express');
-
+const { typeDefs, resolvers } = require('./Schemas');
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// create a new Apollo server and pass in our schema data
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
+
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -15,8 +25,10 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+//app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  
 });
