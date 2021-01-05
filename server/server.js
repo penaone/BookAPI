aaecc19f18d100ff02a11b059f417ec2b0ba9eb4
penaone <penaone@gmail.com>
2021-01-04 21:59:52 -0700

    added line in server.js

--- server/server.js
+++ server/server.js
@@ -1,14 +1,15 @@
-const express = require('express');
 const path = require('path');
-const db = require('./config/connection');
+const { authMiddleware } = require('./utils/auth');
+const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
+
+
 //const routes = require('./routes');
-const { authMiddleware } = require('./utils/auth');
 
-const app = express();
 const PORT = process.env.PORT || 3001;
-
+const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
@@ -17,7 +18,7 @@ const server = new ApolloServer({
 
 server.applyMiddleware({ app });
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
@@ -25,6 +26,9 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 //app.use(routes);
 
 db.once('open', () => {
