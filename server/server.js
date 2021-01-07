20a3c39339048625dcebb61770dc93550c309c7a
penaone <penaone@gmail.com>
2021-01-06 21:14:32 -0700

    add

--- server/server.js
+++ server/server.js
@@ -29,7 +29,8 @@ if (process.env.NODE_ENV === 'production') {
 
 
 // app.use(routes);
-
+app.set('port',PORT)
+console.log(PORT);
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`🌍 Now listening on localhost:${PORT}`);
