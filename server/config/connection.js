5434887b19ac22bf815e7c0b86b6fca5aeae8a0b
penaone <penaone@gmail.com>
2021-01-10 16:31:26 -0700

    Made structure changes

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+mongoose.connect("mongodb+srv://StephenPena:**Fbangi1988@cluster0.sj9l3.mongodb.net/BookAPI?retryWrites=true&w=majority" || 'mongodb://localhost/googlebooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
