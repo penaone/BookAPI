8e75671fd6e8e40a952ceb99f004ffd938cee26e
penaone <penaone@gmail.com>
2021-01-18 21:46:59 -0700

    fixed small error in index

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -150,7 +150,7 @@ const SearchBooks = () => {
                       className='btn-block btn-info'
                       onClick={() => handleSaveBook(book.bookId)}>
                       {savedBookIds?.some((savedBookId) => savedBookId === book.bookId)
-                        ? 'This book has already been saved!'
+                        ? 'This book is now saved!'
                         : 'Save this Book!'}
                     </Button>
                   )}
